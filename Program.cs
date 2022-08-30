9535d5e05d57e5dea5b3d0d988d639ee53c71b7d
Derek Griffin <griffinderek6@gmail.com>
2022-08-30 06:59:50 -0600

    fixed

--- Program.cs
+++ Program.cs
@@ -98,7 +98,7 @@ static bool cont = true;
 
                      randomActivity = activities[randomNumber];
         }
-                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
                 Console.WriteLine();
                bool conti = (Console.ReadLine().ToLower() == "Yes") ? true : false;
     }
